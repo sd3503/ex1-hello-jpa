f687b7fb771d89881896b86ae38e7eba40e02f25
SeonDong <sdkim@tssni.com>
2025-01-15 15:48:23 +0900

    Initial commit

---
+++ src/main/java/hellojap/JapMain.java
@@ -0,0 +1,19 @@
+package hellojap;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JapMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+        emf.close();
+
+
+    }
+}
